d1d3e5fe8ee7669abf80908d5b974b6351e6a74f
lovelorn <cgc1667@sina.com>
2015-04-21 16:09:35 +0800

    初始添加 初始版本： 为2015年1月给河源、北京电信研究院的版本。 带有有效期控制
    Signed-off-by: lovelorn <cgc1667@sina.com>

---
+++ 手机GPS信号转发工具/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace 手机GPS信号转发工具
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 151);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(791, 233);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(113, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(526, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ADB文件路径：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(645, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "浏览...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(518, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "启动";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(145, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "串口号：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(234, 101);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(66, 20);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(336, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "波特率：";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "9600",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBox2.Location = new System.Drawing.Point(395, 101);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(81, 20);
+            this.comboBox2.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button3.Location = new System.Drawing.Point(370, 399);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "清空";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(26, 103);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "串口转发";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 434);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "手机GPS信息转发工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ 手机GPS信号转发工具/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace 手机GPS信号转发工具
+{
+    public partial class Form1 : Form
+    {
+        bool IsWork = false;
+        Process por = new Process();
+        List<string> newlines = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.comboBox1.Items.Clear();
+
+
+            string[] ports = System.IO.Ports.SerialPort.GetPortNames();
+            Array.Sort(ports);
+            this.comboBox1.Items.AddRange(ports);
+
+            this.comboBox1.SelectedIndex = 0;
+            this.comboBox2.SelectedIndex = 3;
+            this.comboBox1.Enabled = this.checkBox1.Checked;
+            this.comboBox2.Enabled = this.checkBox1.Checked;
+
+            if (DateTime.Now.CompareTo(new DateTime(2015, 4, 15, 14, 15, 00)) >= 0)
+            {
+                MessageBox.Show("软件已经过期", "错误");
+                this.Close();
+            }
+
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //por.StandardInput.WriteLine("dir");
+
+            string cmdstring ="\""+ this.textBox2.Text + "\" logcat -v tag -s NMEA2LOG";
+            por.StandardInput.WriteLine(cmdstring);
+            //por.StandardInput.WriteLine("Exit");
+
+            StreamReader sr = por.StandardOutput;
+            char[]buffer = new char[1024];
+            string line = "";
+            while (true)
+            {
+                if (IsWork == false)
+                {
+                    return;
+                } 
+                try
+                {
+                    //int len = sr.Read(buffer, 0, 100);
+                    //newLine = "";
+                    //for (int i = 0; i < len; i++)
+                    //{
+                    //    newLine += buffer[i];
+                    //}
+                    //this.backgroundWorker1.ReportProgress(50);
+
+
+                    line = sr.ReadLine();
+                    line += "\n";
+                    if (line.Contains("$GP"))
+                    {
+                        int Index = line.IndexOf("$GP");
+                        string substring = line.Substring(Index);
+
+                        string[] cmds = substring.Split(new char[] { '$' });
+                        foreach (string singel in cmds)
+                        {
+                            string result = singel;
+                            if (result.Contains("GP"))
+                            {
+                                if (result.Contains("\n"))
+                                {
+                                    int enterIndex = result.IndexOf('\n');
+                                    result = result.Remove(enterIndex);
+                                }
+                                string oneline = "$" + result + "\r\n";
+                                byte[] wtiteBytes = Encoding.Default.GetBytes(oneline);
+                                newlines.Add(oneline);
+                                if(this.serialPort1.IsOpen)
+                                    this.serialPort1.Write(wtiteBytes, 0, wtiteBytes.Length);
+                                this.backgroundWorker1.ReportProgress(50);
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    line = "chu cuo la ";
+                }
+
+
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            int count = newlines.Count;
+            for (int i = 0; i < count; i++)
+            {
+                this.textBox1.AppendText(newlines[0]);
+                newlines.RemoveAt(0);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (IsWork == true)
+            {
+                this.backgroundWorker1.CancelAsync();
+                IsWork = false;
+                this.button2.Text = "开启";
+                por.Close();
+                if(this.serialPort1.IsOpen)
+                    this.serialPort1.Close();
+            }
+            else
+            {
+                if (textBox2.Text.Contains("adb.exe") == false)
+                {
+                    MessageBox.Show("请先选择adb.exe文件", "提示");
+                    return;
+                }
+
+                if (this.comboBox1.SelectedIndex == -1)
+                {
+                    MessageBox.Show("请选择串口号", "提示");
+                    return;
+                }
+                if (this.comboBox2.SelectedIndex == -1)
+                {
+                    MessageBox.Show("请选择波特率", "提示");
+                    return;
+                }
+
+                if (this.checkBox1.Checked)
+                {
+                    this.serialPort1.PortName = this.comboBox1.SelectedItem.ToString();
+                    this.serialPort1.BaudRate = Convert.ToInt32(this.comboBox2.SelectedItem.ToString());
+                    this.serialPort1.Parity = System.IO.Ports.Parity.None;
+                    this.serialPort1.DataBits = 8;
+                    try
+                    {
+                        this.serialPort1.Open();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("串口开启失败！", "提示");
+                        return;
+                    }
+                }
+
+                por.StartInfo.FileName = "cmd.exe";
+                por.StartInfo.UseShellExecute = false;
+                por.StartInfo.RedirectStandardInput = true;
+                por.StartInfo.RedirectStandardOutput = true;
+                por.StartInfo.RedirectStandardError = true;
+                por.StartInfo.CreateNoWindow = true;
+
+                por.Start();
+
+                IsWork = true;
+                try
+                {
+                    this.backgroundWorker1.RunWorkerAsync();
+                }
+                catch
+                {
+                    MessageBox.Show("任务启动失败，请重新启动！程序将自动关闭", "提示");
+                    return;
+                }
+                this.button2.Text = "停止";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "adb.exe|adb.exe";
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.textBox2.Text = ofd.FileName;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.comboBox1.Enabled = this.checkBox1.Checked;
+            this.comboBox2.Enabled = this.checkBox1.Checked;
+        }
+
+
+
+
+    }
+}
